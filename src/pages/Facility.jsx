3536b4fd44e2cb38aa58231996c23198d2aedcae
Akintola97 <84879342+Akintola97@users.noreply.github.com>
2022-12-09 13:49:46 -0600

    update

--- src/pages/Facility.jsx
+++ src/pages/Facility.jsx
@@ -25,8 +25,8 @@ const Facility = ({data}) => {
   return (
     <>
     <div className='text-black w-full h-full bg-gray-200'>
-      <h1 className='uppercase text-center font-bold text-[6vmin] pt-4'>Surge to new levels Facility</h1>
-      <p className='text-center text-[2.8vmin] p-3'>With over 12,000 square feet; Surge to New Levels boasts one of the largest spaces for powerlifting, strongman, and boxing in the entire Chicagoland area! Outfitted with all competition grade equipment, you will find absolutely everything you need for your sport at our gym! Best of all we have one of the largest and friendliest communities around! </p>
+      <h1 className='uppercase text-center font-bold text-[4vmin] pt-4'>Surge to new levels Facility</h1>
+      <p className='text-center text-[2.5vmin] p-3'>With over 12,000 square feet; Surge to New Levels boasts one of the largest spaces for powerlifting, strongman, and boxing in the entire Chicagoland area! Outfitted with all competition grade equipment, you will find absolutely everything you need for your sport at our gym! Best of all we have one of the largest and friendliest communities around! </p>
      
        {/* <img className='w-[32vw] p-2' src={squat_facility_female} />
        <img className='w-[32vw] p-2' src={loading_plates} /> 
@@ -49,145 +49,142 @@ const Facility = ({data}) => {
 </div>
       <div className='w-full h-full bg-gray-200'>
         <div>
-      <h1 className='uppercase text-center font-bold text-[6vmin] pt-5 pb-4'>The Equipment</h1>
+      <h1 className='uppercase text-center font-bold text-[4vmin] pt-5 pb-4'>The Equipment</h1>
       </div>
       <div className='flex w-full h-full md:flex-row flex-col justify-center text-center pb-5'>
       <div className='w-full h-full'>
-        <h3 className='font-bold text-[4vmin]'>Specialty Barbells</h3>
-            <p className='text-[2.8vmin]'>Texas Squat Bar (2) 25KG/55LBS</p>
-            <p className='text-[2.8vmin]'>Kabuki Strength Squat Bar (2) 25KG/55LBS</p>
-            <p className='text-[2.8vmin]'>Kabuki Transformer Bar (1) 45LBS</p>
-            <p className='text-[2.8vmin]'>Westside Bulldog Squat Bar (1) 30KG/66LBS</p>
-            <p className='text-[2.8vmin]'>Texas mastadon Bar (1) 60LBS</p>
-            <p className='text-[2.8vmin]'>Kabuki Deadlift Bar (1) 20KG/45LBS</p>
-            <p className='text-[2.8vmin]'>Texas Deadlift Bar (3) 20KG/45LBS</p>
-            <p className='text-[2.8vmin]'>Ohio Deadlift Bar (5) 20KG/45LBS</p>
-            <p className='text-[2.8vmin]'>Texas Power Bar (2) 20KG/45LBS</p>
-            <p className='text-[2.8vmin]'>Ohio Power Bar Pounds (2) 45LBS</p>
-            <p className='text-[2.8vmin]'>Ohio Power Bar Kilos (1) 20KG</p>
-            <p className='text-[2.8vmin]'>Kabuki Strength Power Bar (1) 20KG/45LBS</p>
-            <p className='text-[2.8vmin]'>Elite FTS Power Bar (1) 45LBS</p>
-            <p className='text-[2.8vmin]'>Ohio Olympic Bar (1) 20KG/45LBS</p>
-            <p className='text-[2.8vmin]'>Rogue Fitness Bella Bar (1) 15KG/33LBS</p>
-            <p className='text-[2.8vmin]'>Kabuki Strength Duffalo Bar (1) 55LBS</p>
-            <p className='text-[2.8vmin]'>Kabuki Strength Shoulder Rok (1)</p>
-            <p className='text-[2.8vmin]'>Rogue Fintess Multi Grip Bar (1) 45LBS</p>
-            <p className='text-[2.8vmin]'>Elite FTS Safety Squat Bar (2) 65LBS</p>
-            <p className='text-[2.8vmin]'>Black Widow Cambered Bar (1) 38LBS</p>
-            <p className='text-[2.8vmin]'>BandBell Earthquake Bar (1) 6LBS</p>
+        <h3 className='font-bold text-[3.5vmin]'>Specialty Barbells</h3>
+            <p className='text-[2.3vmin]'>Texas Squat Bar (2) 25KG/55LBS</p>
+            <p className='text-[2.3vmin]'>Kabuki Strength Squat Bar (2) 25KG/55LBS</p>
+            <p className='text-[2.3vmin]'>Kabuki Transformer Bar (1) 45LBS</p>
+            <p className='text-[2.3vmin]'>Westside Bulldog Squat Bar (1) 30KG/66LBS</p>
+            <p className='text-[2.3vmin]'>Texas mastadon Bar (1) 60LBS</p>
+            <p className='text-[2.3vmin]'>Kabuki Deadlift Bar (1) 20KG/45LBS</p>
+            <p className='text-[2.3vmin]'>Texas Deadlift Bar (3) 20KG/45LBS</p>
+            <p className='text-[2.3vmin]'>Ohio Deadlift Bar (5) 20KG/45LBS</p>
+            <p className='text-[2.3vmin]'>Texas Power Bar (2) 20KG/45LBS</p>
+            <p className='text-[2.3vmin]'>Ohio Power Bar Pounds (2) 45LBS</p>
+            <p className='text-[2.3vmin]'>Ohio Power Bar Kilos (1) 20KG</p>
+            <p className='text-[2.3vmin]'>Kabuki Strength Power Bar (1) 20KG/45LBS</p>
+            <p className='text-[2.3vmin]'>Elite FTS Power Bar (1) 45LBS</p>
+            <p className='text-[2.3vmin]'>Ohio Olympic Bar (1) 20KG/45LBS</p>
+            <p className='text-[2.3vmin]'>Rogue Fitness Bella Bar (1) 15KG/33LBS</p>
+            <p className='text-[2.3vmin]'>Kabuki Strength Duffalo Bar (1) 55LBS</p>
+            <p className='text-[2.3vmin]'>Kabuki Strength Shoulder Rok (1)</p>
+            <p className='text-[2.3vmin]'>Rogue Fintess Multi Grip Bar (1) 45LBS</p>
+            <p className='text-[2.3vmin]'>Elite FTS Safety Squat Bar (2) 65LBS</p>
+            <p className='text-[2.3vmin]'>Black Widow Cambered Bar (1) 38LBS</p>
+            <p className='text-[2.3vmin]'>BandBell Earthquake Bar (1) 6LBS</p>
         </div>
         <div className='w-full h-full'>
-        <h3 className='font-bold text-[4vmin]'>Strongman</h3>
-          
-            <p className='text-[2.8vmin]'>Stone Loading Platform (4 Heights)</p>
-            <p className='text-[2.8vmin]'>Rogue Fitness Adjustable Jerk Blocks</p>
-            <p className='text-[2.8vmin]'>Rogue Fitness Strongman Yoke 185LBS</p>
-            <p className='text-[2.8vmin]'>Atlas Stones - 100LBS to 400LBS</p>
-            <p className='text-[2.8vmin]'>Mike Bartos 20" Stone of Steel 135LBS</p>
-            <p className='text-[2.8vmin]'>Mike Bartos 17" Stone of Steel 110LBS</p>
-            <p className='text-[2.8vmin]'>Mike Bartos 12" Circus Dumbbell 139LBS</p>
-            <p className='text-[2.8vmin]'>Mike Bartos 10" Circus Dumbbell 75LBS</p>
-            <p className='text-[2.8vmin]'>Mike Bartos Power Keg 130LBS</p>
-            <p className='text-[2.8vmin]'>Mike Bartos DeadSled 150LBS</p>
-            <p className='text-[2.8vmin]'>MBPC Farmer Walk Handles 68LBS</p>
-            <p className='text-[2.8vmin]'>Mike Bartos 12" BOSS Log 230LBS</p>
-            <p className='text-[2.8vmin]'>Titan Fitness Strongman Log 10" 75LBS</p>
-            <p className='text-[2.8vmin]'>Rogue Fitness Log 8" 51LBS</p>
-            <p className='text-[2.8vmin]'>Rogue Fitness Log 12" 130LBS</p>
-            <p className='text-[2.8vmin]'>Rogue Fitness Axle 25LBS</p>
-            <p className='text-[2.8vmin]'>Strong Sand Bags 100LBS, 150LBS, 200LBS, 250LBS, 300LBS, 400LBS</p>
-            <p className='text-[2.8vmin]'>Elite FTS Farmer Carry Handles (20lbs)</p>
-            <p className='text-[2.8vmin]'>Titan Fitness Viking Press Attachment</p>
-            <p className='text-[2.8vmin]'>Titan Fitness Hussafell 110LBS</p>
-            <p className='text-[2.8vmin]'>Mouser Block</p>
-            <p className='text-[2.8vmin]'>Black Widow Trap Bar 35LBS</p>
-            <p className='text-[2.8vmin]'>Frame Carry</p>
-            <p className='text-[2.8vmin]'>Tires(250LBS and 550LBS)</p>
+        <h3 className='font-bold text-[3.5vmin] '>Strongman</h3>
+            <p className='text-[2.3vmin]'>Stone Loading Platform (4 Heights)</p>
+            <p className='text-[2.3vmin]'>Rogue Fitness Adjustable Jerk Blocks</p>
+            <p className='text-[2.3vmin]'>Rogue Fitness Strongman Yoke 185LBS</p>
+            <p className='text-[2.3vmin]'>Atlas Stones - 100LBS to 400LBS</p>
+            <p className='text-[2.3vmin]'>Mike Bartos 20" Stone of Steel 135LBS</p>
+            <p className='text-[2.3vmin]'>Mike Bartos 17" Stone of Steel 110LBS</p>
+            <p className='text-[2.3vmin]'>Mike Bartos 12" Circus Dumbbell 139LBS</p>
+            <p className='text-[2.3vmin]'>Mike Bartos 10" Circus Dumbbell 75LBS</p>
+            <p className='text-[2.3vmin]'>Mike Bartos Power Keg 130LBS</p>
+            <p className='text-[2.3vmin]'>Mike Bartos DeadSled 150LBS</p>
+            <p className='text-[2.3vmin]'>MBPC Farmer Walk Handles 68LBS</p>
+            <p className='text-[2.3vmin]'>Mike Bartos 12" BOSS Log 230LBS</p>
+            <p className='text-[2.3vmin]'>Titan Fitness Strongman Log 10" 75LBS</p>
+            <p className='text-[2.3vmin]'>Rogue Fitness Log 8" 51LBS</p>
+            <p className='text-[2.3vmin]'>Rogue Fitness Log 12" 130LBS</p>
+            <p className='text-[2.3vmin]'>Rogue Fitness Axle 25LBS</p>
+            <p className='text-[2.3vmin]'>Strong Sand Bags 100LBS, 150LBS, 200LBS, 250LBS, 300LBS, 400LBS</p>
+            <p className='text-[2.3vmin]'>Elite FTS Farmer Carry Handles (20lbs)</p>
+            <p className='text-[2.3vmin]'>Titan Fitness Viking Press Attachment</p>
+            <p className='text-[2.3vmin]'>Titan Fitness Hussafell 110LBS</p>
+            <p className='text-[2.3vmin]'>Mouser Block</p>
+            <p className='text-[2.3vmin]'>Black Widow Trap Bar 35LBS</p>
+            <p className='text-[2.3vmin]'>Frame Carry</p>
+            <p className='text-[2.3vmin]'>Tires(250LBS and 550LBS)</p>
        </div>
   
        <div className='w-full h-full'>
-        <h3 className='font-bold text-[4vmin]'>Boxing</h3>
-        
-          <p className='text-[2.8vmin]'>22X22FT Professional Boxing Ring</p>
-          <p className='text-[2.8vmin]'>Ringside 100LB Heavy Bags (2)</p>
-          <p className='text-[2.8vmin]'>Rival 200LB Heavy Bag (1)</p>
-          <p className='text-[2.8vmin]'>Cleto Reyes Wrecking Ball (1)</p>
-          <p className='text-[2.8vmin]'>Aqua Bag 200LBS (1)</p>
-          <p className='text-[2.8vmin]'>Title Cobra Reflex Bag (1)</p>
-          <p className='text-[2.8vmin]'>Title Reflex Bar (1)</p>
-          <p className='text-[2.8vmin]'>Speed Bag (1)</p>
-          <p className='text-[2.8vmin]'>Double End Bag (1)</p>
+        <h3 className='font-bold text-[3.5vmin] '>Boxing</h3>
+          <p className='text-[2.3vmin]'>22X22FT Professional Boxing Ring</p>
+          <p className='text-[2.3vmin]'>Ringside 100LB Heavy Bags (2)</p>
+          <p className='text-[2.3vmin]'>Rival 200LB Heavy Bag (1)</p>
+          <p className='text-[2.3vmin]'>Cleto Reyes Wrecking Ball (1)</p>
+          <p className='text-[2.3vmin]'>Aqua Bag 200LBS (1)</p>
+          <p className='text-[2.3vmin]'>Title Cobra Reflex Bag (1)</p>
+          <p className='text-[2.3vmin]'>Title Reflex Bar (1)</p>
+          <p className='text-[2.3vmin]'>Speed Bag (1)</p>
+          <p className='text-[2.3vmin]'>Double End Bag (1)</p>
 
         </div>
         <div className='w-full h-full'>
-        <h3 className='font-bold text-[4vmin]'>Machines</h3>
-        
-           <p className='text-[2.8vmin]'>Precor Icarian 8 Multi-Stack Cable Station</p>
-           <p className='text-[2.8vmin]'>Rogue Fitness Rhino Belt Squat Machine</p>
-           <p className='text-[2.8vmin]'>Reverse Hyper </p>
-           <p className='text-[2.8vmin]'>Seal Row Machine</p>
-           <p className='text-[2.8vmin]'>Rogue Fitness Glute Ham Raise</p>
-           <p className='text-[2.8vmin]'>Legend Fitness Leg Press</p>
-           <p className='text-[2.8vmin]'>Elite FTS Leg Ext. Leg Curl</p>
-           <p className='text-[2.8vmin]'>Sissy Squat Machine</p>
+        <h3 className='font-bold text-[3.5vmin] '>Machines</h3>
+           <p className='text-[2.3vmin]'>Precor Icarian 8 Multi-Stack Cable Station</p>
+           <p className='text-[2.3vmin]'>Rogue Fitness Rhino Belt Squat Machine</p>
+           <p className='text-[2.3vmin]'>Reverse Hyper </p>
+           <p className='text-[2.3vmin]'>Seal Row Machine</p>
+           <p className='text-[2.3vmin]'>Rogue Fitness Glute Ham Raise</p>
+           <p className='text-[2.3vmin]'>Legend Fitness Leg Press</p>
+           <p className='text-[2.3vmin]'>Elite FTS Leg Ext. Leg Curl</p>
+           <p className='text-[2.3vmin]'>Sissy Squat Machine</p>
        </div>
         </div>
         <div className='flex w-full h-full md:flex-row flex-col justify-center text-center pb-10'>
         <div className='w-full h-full'>
-        <h3 className='font-bold text-[4vmin]'>Accessories, Mobility, <br></br>and Misc.</h3>
-            <p className='text-[2.8vmin]'>Kabuki Strength Boomstick</p>
-            <p className='text-[2.8vmin]'>Elite FTS Bands Full Set (2)</p>
-            <p className='text-[2.8vmin]'>Black Widow 5/8" Chains (6)</p>
-            <p className='text-[2.8vmin]'>TRX Suspension System</p>
-            <p className='text-[2.8vmin]'>Rogue Fitness Dip Station</p>
-            <p className='text-[2.8vmin]'>Rogue Fitness Landmine</p>
-            <p className='text-[2.8vmin]'>DC Blocks (8)</p>
-            <p className='text-[2.8vmin]'>Deadlift Jack (3)</p>
-            <p className='text-[2.8vmin]'>Chalk Bowl and Chalk (2)</p>
-            <p className='text-[2.8vmin]'>Ammonia</p>
-            <p className='text-[2.8vmin]'>Rumble Roller</p>
-            <p className='text-[2.8vmin]'>Tiger Tail Roller</p>
-            <p className='text-[2.8vmin]'>Acumobility Level 2 Ball (2)</p>
-            <p className='text-[2.8vmin]'>Slingshot Hip Circle</p>
-            <p className='text-[2.8vmin]'>Slingshot Mad Boar</p>
-            <p className='text-[2.8vmin]'>Slingshot Knee Wraps</p>
-            <p className='text-[2.8vmin]'>Slingshot Wrist Wraps</p>
+        <h3 className='font-bold text-[3.5vmin] '>Accessories, Mobility, <br></br>and Misc.</h3>
+            <p className='text-[2.3vmin]'>Kabuki Strength Boomstick</p>
+            <p className='text-[2.3vmin]'>Elite FTS Bands Full Set (2)</p>
+            <p className='text-[2.3vmin]'>Black Widow 5/8" Chains (6)</p>
+            <p className='text-[2.3vmin]'>TRX Suspension System</p>
+            <p className='text-[2.3vmin]'>Rogue Fitness Dip Station</p>
+            <p className='text-[2.3vmin]'>Rogue Fitness Landmine</p>
+            <p className='text-[2.3vmin]'>DC Blocks (8)</p>
+            <p className='text-[2.3vmin]'>Deadlift Jack (3)</p>
+            <p className='text-[2.3vmin]'>Chalk Bowl and Chalk (2)</p>
+            <p className='text-[2.3vmin]'>Ammonia</p>
+            <p className='text-[2.3vmin]'>Rumble Roller</p>
+            <p className='text-[2.3vmin]'>Tiger Tail Roller</p>
+            <p className='text-[2.3vmin]'>Acumobility Level 2 Ball (2)</p>
+            <p className='text-[2.3vmin]'>Slingshot Hip Circle</p>
+            <p className='text-[2.3vmin]'>Slingshot Mad Boar</p>
+            <p className='text-[2.3vmin]'>Slingshot Knee Wraps</p>
+            <p className='text-[2.3vmin]'>Slingshot Wrist Wraps</p>
         </div>
         <div className='w-full h-full'>
-        <h3 className='font-bold text-[4vmin]'>Racks, Benches,<br></br> Platforms</h3>
+        <h3 className='font-bold text-[3.5vmin] '>Racks, Benches,<br></br> Platforms</h3>
           
-            <p className='text-[2.8vmin]'>Powerlifting Monolift (2)</p>
-            <p className='text-[2.8vmin]'>Ghost Combo Rack (2)</p>
-            <p className='text-[2.8vmin]'>Elite FTS Competition Combo Rack (2)</p>
-            <p className='text-[2.8vmin]'>Rogue Fitness Combo Rack (2)</p>
-            <p className='text-[2.8vmin]'>Wall Rigs (3)</p>
-            <p className='text-[2.8vmin]'>Rogue Fitness Monster Lite Rack (4)</p>
-            <p className='text-[2.8vmin]'>Elite FTS National Competition Bench</p>
-            <p className='text-[2.8vmin]'>4x8 Deadlift Platform with Band Pegs (6)</p>
-            <p className='text-[2.8vmin]'>Elite FTS Adjustable 0-90 Bench (2)</p>
-            <p className='text-[2.8vmin]'>Monster Utility Benches (2)</p>
+            <p className='text-[2.3vmin]'>Powerlifting Monolift (2)</p>
+            <p className='text-[2.3vmin]'>Ghost Combo Rack (2)</p>
+            <p className='text-[2.3vmin]'>Elite FTS Competition Combo Rack (2)</p>
+            <p className='text-[2.3vmin]'>Rogue Fitness Combo Rack (2)</p>
+            <p className='text-[2.3vmin]'>Wall Rigs (3)</p>
+            <p className='text-[2.3vmin]'>Rogue Fitness Monster Lite Rack (4)</p>
+            <p className='text-[2.3vmin]'>Elite FTS National Competition Bench</p>
+            <p className='text-[2.3vmin]'>4x8 Deadlift Platform with Band Pegs (6)</p>
+            <p className='text-[2.3vmin]'>Elite FTS Adjustable 0-90 Bench (2)</p>
+            <p className='text-[2.3vmin]'>Monster Utility Benches (2)</p>
         </div>
         <div className='w-full h-full'>
-        <h3 className='font-bold text-[4vmin]'>Cardio and Conditioning Equipment</h3>
-        <p className='text-[2.8vmin]'>Life Fitness Treadmill</p>
-        <p className='text-[2.8vmin]'>Step Mill Stairmaster</p>
-        <p className='text-[2.8vmin]'>Assault Air Bike</p>
-        <p className='text-[2.8vmin]'>Concept 2 Rower</p>
-        <p className='text-[2.8vmin]'>60ft of Athletic Turf</p>
-        <p className='text-[2.8vmin]'>Butcher Prowler 75LBS</p>
-        <p className='text-[2.8vmin]'>Fat Boy Sled 38LBS</p>
-        <p className='text-[2.8vmin]'>Plyo Boxes</p>
-        <p className='text-[2.8vmin]'>Battle Ropes</p>
-        <p className='text-[2.8vmin]'>Agility Cones</p>
+        <h3 className='font-bold text-[3.5vmin] '>Cardio and Conditioning Equipment</h3>
+        <p className='text-[2.3vmin]'>Life Fitness Treadmill</p>
+        <p className='text-[2.3vmin]'>Step Mill Stairmaster</p>
+        <p className='text-[2.3vmin]'>Assault Air Bike</p>
+        <p className='text-[2.3vmin]'>Concept 2 Rower</p>
+        <p className='text-[2.3vmin]'>60ft of Athletic Turf</p>
+        <p className='text-[2.3vmin]'>Butcher Prowler 75LBS</p>
+        <p className='text-[2.3vmin]'>Fat Boy Sled 38LBS</p>
+        <p className='text-[2.3vmin]'>Plyo Boxes</p>
+        <p className='text-[2.3vmin]'>Battle Ropes</p>
+        <p className='text-[2.3vmin]'>Agility Cones</p>
        </div>
        <div className='w-full h-full'>
-        <h3 className='font-bold text-[4vmin]'>Strength Equipment and Weights</h3>
-        <p className='text-[2.8vmin]'>IPF Certified Kilo Plates (3 Full Sets)</p>
-        <p className='text-[2.8vmin]'>Ivanko Competition Collars </p>
-        <p className='text-[2.8vmin]'>Ivanko Pro Style Dumbbells</p>
-        <p className='text-[2.8vmin]'>Cast Iron Standard Plates</p>
-        <p className='text-[2.8vmin]'>Rogue Fitness Kettlebell Set</p>
-        <p className='text-[2.8vmin]'>Rogue Fitness Bumper Plates</p>
+        <h3 className='font-bold text-[3.5vmin]'>Strength Equipment and Weights</h3>
+        <p className='text-[2.3vmin]'>IPF Certified Kilo Plates (3 Full Sets)</p>
+        <p className='text-[2.3vmin]'>Ivanko Competition Collars </p>
+        <p className='text-[2.3vmin]'>Ivanko Pro Style Dumbbells</p>
+        <p className='text-[2.3vmin]'>Cast Iron Standard Plates</p>
+        <p className='text-[2.3vmin]'>Rogue Fitness Kettlebell Set</p>
+        <p className='text-[2.3vmin]'>Rogue Fitness Bumper Plates</p>
             </div>
         </div>
         

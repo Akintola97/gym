06d45a4eb1b37097a6b5c8f77405c2bb2928996f
Akintola97 <84879342+Akintola97@users.noreply.github.com>
2022-12-09 18:41:44 -0600

    update

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -14,12 +14,12 @@ function Navigation() {
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto items-center p-0">
             {/* <Nav.Link><Link class = 'text-decoration-none text-white' to = '/Team'><h5 className='hover:text-red-500'>Team</h5></Link></Nav.Link> */}
-            <Nav.Link><Link class = 'text-decoration-none text-white' to = '/Facility'><h5 className='hover:text-red-500 font-bold'>Facility</h5></Link></Nav.Link>
-            <Nav.Link><Link class = 'text-decoration-none text-white' to = '/Memberships'><h5 className='hover:text-red-500 font-bold'>Memberships</h5></Link></Nav.Link>
+            <Nav.Link eventKey="1"><Link class = 'text-decoration-none text-white' to = '/Facility'><h5 className='hover:text-red-500 font-bold'>Facility</h5></Link></Nav.Link>
+            <Nav.Link eventKey="2"><Link class = 'text-decoration-none text-white' to = '/Memberships'><h5 className='hover:text-red-500 font-bold'>Memberships</h5></Link></Nav.Link>
             {/* <Nav.Link><Link class = 'text-decoration-none text-white' to = '/Coaching'><h5 className='hover:text-red-500 font-bold'>Coaching</h5></Link></Nav.Link> */}
-            <Nav.Link><Link class = 'text-decoration-none text-white' to = '/Boxing'><h5 className='hover:text-red-500 font-bold'>Boxing</h5></Link></Nav.Link>
-           <Nav.Link><Link class = 'text-decoration-none text-white' to = '/Rhino'><h5 className='hover:text-red-500 font-bold'>Rhino's Revenge</h5></Link></Nav.Link>
-            <Nav.Link><Link class = 'text-decoration-none text-white' to = '/Cart'><h5 className='hover:text-red-500 font-bold'><AiOutlineShoppingCart/></h5></Link></Nav.Link>
+            <Nav.Link eventKey="3"><Link class = 'text-decoration-none text-white' to = '/Boxing'><h5 className='hover:text-red-500 font-bold'>Boxing</h5></Link></Nav.Link>
+           <Nav.Link eventKey="4"><Link class = 'text-decoration-none text-white' to = '/Rhino'><h5 className='hover:text-red-500 font-bold'>Rhino's Revenge</h5></Link></Nav.Link>
+            <Nav.Link eventKey="5"><Link class = 'text-decoration-none text-white' to = '/Cart'><h5 className='hover:text-red-500 font-bold'><AiOutlineShoppingCart/></h5></Link></Nav.Link>
           </Nav>
           <Nav>
           </Nav>
